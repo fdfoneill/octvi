951690c59ad6b248a4c5cf2754dfbc87bb69b6e9
fdfoneill <fdfoneill@gmail.com>
2020-01-30 10:30:46 -0500

    Fixed setup.py; added manifest

--- setup.py
+++ setup.py
@@ -6,28 +6,24 @@ from setuptools import setup
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
+def readme():
+	with open('README.md') as f:
+		return f.read()
 
 setup(name='octvi',
 		version='1.0.0',
 		description='MODIS 8-day NDVI Downloader',
+		long_description=readme(),
 		author="F. Dan O'Neill",
 		author_email='fdfoneill@gmail.com',
 		license='MIT',
-		packages=find_packages(),
+		packages=['octvi'],
 		include_package_data=True,
 		# third-party dependencies
 		install_requires=[
-						'csv',
-						'datetime',
-						'gdal',
-						'h5py',
-						'io',
-						'logging',
-						'numpy',
-						'shutil',
-						'ssl',
-						'subprocess',
-						'urllib'
-						],
-		entry_points={}
-	  )
+			'numpy',
+			'h5py',
+			'gdal'
+			],
+		zip_safe=False
+		)
