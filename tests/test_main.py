619ff51440719bdc194da5a176713f7eb8d8b5d8
fdfoneill <fdfoneill@gmail.com>
2020-01-30 15:29:12 -0500

    Added tests of __init__.py

---
+++ tests/test_main.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+import octvi
+import os
+
+class TestMosaic(TestCase):
+
+	def test_mosaicking(self):
+		tempDir = os.path.dirname(__file__)
+		urls = [u[0] for u in octvi.url.getUrls("MOD09Q1","2019-01-01",tiles=['h00v08','h00v09'])]
+		try:
+			idealMosaicName = "mosaicTest.tif"
+			hdfs = [octvi.url.pull(u,tempDir) for u in urls]
+			ndvis = [octvi.extract.ndviToRaster(h,h.replace(".hdf",".ndvi.tif")) for h in hdfs]
+			mosaicPath = octvi.mosaic(ndvis,os.path.join(tempDir,idealMosaicName))
+			self.assertEqual(os.path.basename(mosaicPath),idealMosaicName)
+		finally:
+			for f in hdfs:
+				os.remove(f)
+			for f in ndvis:
+				os.remove(f)
+			os.remove(mosaicPath)
+
+class TestCmgNdvi(TestCase):
+	
+	def test_compositing(self):
+		idealOutPath = os.path.join(os.path.dirname(__file__),'compositeTest.tif')
+		res = cmgNdvi("2019-01-01",idealOutPath,overwrite=True)
+		self.assertIsInstance(res,str)
+		self.assertIsEqual(os.path.basename(res),'compositeTest.tif')
+		os.remove(res)
+
+class TestGlobalNdvi(TestCase):
+	
+	def test_unsupportedProduct(self):
+		customError = False
+		try:
+			octvi.globalNdvi("MOD66F8","2019-01-01",os.path.join(os.path.dirname(__file__),"unsupported.tif"),overwrite=True)
+			os.remove(os.path.join(os.path.dirname(__file__),"unsupported.tif"))
+		except octvi.exceptions.UnsupportedError:
+			customError = True
+		self.assertTrue(customError)
